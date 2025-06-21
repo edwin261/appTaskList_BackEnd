1a71eb2d183315b9d2bf7e28994d6f1c3d626d72
Edwin Rafael Marquez Melendez <edwin.marquez@globalmvm.com>
2025-06-21 02:54:24 -0500

    Se añaden test a controladores

---
+++ src/test/TaskController.test.js
@@ -0,0 +1,48 @@
+import express from "express"
+import request from "supertest"
+import taskController from "../controllers/Task/task.js";
+
+const app = express();
+app.use(express.json());
+
+app.get('/task', taskController.getTaskRecords)
+app.post('/task', taskController.addTaskRecord)
+app.put('/task', taskController.putTaskRecord)
+app.delete('/task/:id', taskController.deleteTaskRecord);
+
+const taskRecord = [{
+    id: 1,
+    title: "Tarea prueba",
+    estado: "Pendiente"
+}]
+
+describe("controller method 'addTaskRecord()'", () => {
+    it("Crear un nuevo registro de tarea", () => {
+        const firstTaskRecord = taskRecord[0];
+        const response = request(app).post('/task').send(firstTaskRecord);
+        expect(response).toMatchObject(/firstTaskRecord/);
+    });
+});
+
+describe("controller method 'getTaskRecords()'", () => {
+    it("Obtener el listado de tareas creadas", () => {
+        const response = request(app).get('/task');
+        expect(response).not.toEqual(0);
+    });
+});
+
+describe("controller method 'putTaskRecord()'", () => {
+    it("editar un registro del listado de tareas creadas", () => {
+        const firstTaskRecord = taskRecord[0];
+        firstTaskRecord.estado = "Completado";
+        const response = request(app).put('/task').send(firstTaskRecord);
+        expect(response).toMatchObject(/firstTaskRecord/);
+    });
+});
+
+describe("controller method 'deleteTaskRecord()'", () => {
+    it("eliminar un registro del listado de tareas creadas", () => {
+        const response = request(app).delete('/task/1');
+        expect(response).toMatchObject(/Delete/);
+    });
+});
