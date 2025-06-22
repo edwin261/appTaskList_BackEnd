5ae3a420f7b57e96026e05f6ef2622cc7fe5cc28
Edwin Rafael Marquez Melendez <edwin.marquez@globalmvm.com>
2025-06-21 21:14:12 -0500

    Mejorsa en test unitarios de controlador Task.js

--- src/test/TaskController.test.js
+++ src/test/TaskController.test.js
@@ -17,32 +17,35 @@ const taskRecord = [{
 }]
 
 describe("controller method 'addTaskRecord()'", () => {
-    it("Crear un nuevo registro de tarea", () => {
+    it("Crear un nuevo registro de tarea", async () => {
         const firstTaskRecord = taskRecord[0];
-        const response = request(app).post('/task').send(firstTaskRecord);
-        expect(response).toMatchObject(/firstTaskRecord/);
+        const response = await request(app).post('/task').send(firstTaskRecord);
+        expect(response.statusCode).toBe(201);
+        expect(response.body).toStrictEqual(firstTaskRecord);
     });
 });
 
 describe("controller method 'getTaskRecords()'", () => {
-    it("Obtener el listado de tareas creadas", () => {
-        const response = request(app).get('/task');
+    it("Obtener el listado de tareas creadas", async () => {
+        const response = await request(app).get('/task');
+        expect(response.statusCode).toBe(200);
         expect(response).not.toEqual(0);
     });
 });
 
 describe("controller method 'putTaskRecord()'", () => {
-    it("editar un registro del listado de tareas creadas", () => {
+    it("editar un registro del listado de tareas creadas", async () => {
         const firstTaskRecord = taskRecord[0];
         firstTaskRecord.estado = "Completado";
-        const response = request(app).put('/task').send(firstTaskRecord);
-        expect(response).toMatchObject(/firstTaskRecord/);
+        const response = await request(app).put('/task').send(firstTaskRecord);
+        expect(response.statusCode).toBe(200);
+        expect(response.body[0].estado).toBe("Completado");
     });
 });
 
 describe("controller method 'deleteTaskRecord()'", () => {
-    it("eliminar un registro del listado de tareas creadas", () => {
-        const response = request(app).delete('/task/1');
-        expect(response).toMatchObject(/Delete/);
+    it("eliminar un registro del listado de tareas creadas", async () => {
+        const response = await request(app).delete('/task/1');
+        expect(response.statusCode).toBe(204);
     });
 });
